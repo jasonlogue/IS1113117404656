ea8ec547221cc563da1133894cce6010795de36f
Jason Logue <117404656@umail.ucc.ie>
2018-02-08 20:07:54 +0000

    ebus2

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -3,55 +3,57 @@
     <head>
         <title>Select Product</title>
         
-        <!-- JQuery -->
-        <script src="https//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="cost_calc.js"></script>
+
+    </head>
+    
+    <body>
         
-        </head>  
-        
-        <body>
+            <h4>Select a Product</h4>
             
-            <h4> Select a Product</h4>
+            <br/>
             
-            </br>
             <form method="POST" action="Ebus2.php">
-                
-                <Label for="salesforce">
-                <input type="radio"  id="salesforce" name="product" checked onClick="disablebtnproceed()"/>
+              
+              <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
                 SalesForce @ $100
-                </label>
-                
-                </br>
-                
-                 <Label for="aws">
-                <input type="radio"  id="aws" name="product" onClick="disablebtnproceed()"/>
+              </label>
+              
+              <br/>
+              
+              <label for="aws">
+                <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
                 AWS @ $300
-                </label>
-                
-                </br>
-                </br>
-                
-                 <Label for="subtotal">
-                     Sub Total
-                <input type="text"  id="subtotal" value="0.00" readonly/>
-                </label>
-                
-                </br>
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="subtotal">
+                Sub Total
+                <input type="text" id="subtotal" value="0.00" readonly/>
+              </label>
+              
+              <br/>
+              
+              <label for="total">
+                Total
+                <input type="text" id="total" value="0.00" readonly/>
+              </label>
+    
+              <br/>
+              
+              <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
             
-                <Label for="total">
-                    Total
-                <input type="text"  id="total" value="0.00" readonly/>
-                </label>
-                
-                </br>
-                
-                <button type="submit" id="btnProceed" disabled> Add to Shopping Cart</button>
-                
             </form>
             
-            </br>
+            <br/>
             <button onClick="calcSub()">Calculate Cost</button>
             <a role="button" href="Ebus1.php">Clear Choice</a>
             
-        </body>
+    
+    </body>
 </html>
--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -4,28 +4,29 @@ function calcSub(){
     
     var argSubTotal;
     
-    if(document.getElementById('salesforce').checked)
-        argSubTotal = 100;
+    if(document.getElementById('salesforce').checked) {
+      argSubTotal = 100;
     }
     else {
-        argSubTotal = 300;
+      argSubTotal = 300;
     }
     
     display(argSubTotal);
 }
 
 function display(parm1){
-    
-    document.getElementById("subtotal").value = parm1;
-    document.getElementById("total").value = parm1;
-    
-    enablebtnProceed();
+  
+  document.getElementById("subtotal").value = parm1;
+  document.getElementById("total").value = parm1;
+        
+  enablebtnProceed();
 }
 
-function enablebtnProceed(){
-    $("btnProceed").prop("disabled", false);
-}
 
 function enablebtnProceed(){
-    $("btnProceed").prop("disabled", true);
-}    
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed() {
+    $('#btnProceed').prop('disabled', true);
+}
